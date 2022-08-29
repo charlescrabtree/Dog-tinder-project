5eaa780ff6bcb9aa5e10010f1a53be63f32436f3
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-08-29 14:08:49 -0700

    fixed pages and pathing

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://mtegvpmustvqjcrpqjft.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10ZWd2cG11c3R2cWpjcnBxamZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTk2MzgyMTYsImV4cCI6MTk3NTIxNDIxNn0.1qATbqaxyJY3HmYMZsX0LcLV6_XXcgd_qnE96O4JeR8';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
