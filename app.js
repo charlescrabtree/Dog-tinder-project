5f39ac55d489aca8da70806c1b023e8e0aa53891
Tanner Richards <Tanner.richards990@gmail.com>
2022-09-01 12:11:02 -0700

    added a interactive nav bar to each html page, with correct pathing for navigating to different pages

--- app.js
+++ app.js
@@ -1,37 +1,45 @@
-import { checkAuth, signOutUser, getUserById, getAllUsers, } from './fetch-utils.js';
-import { renderAllUsers } from './render-function.js';
+import { checkAuth, signOutUser, } from './fetch-utils.js';
+// import { renderAllUsers } from './render-function.js';
 
 checkAuth();
-const user = checkAuth();
 
 
-const userContainerEl = document.getElementById('users-container');
+
+
 const signOutLink = document.getElementById('sign-out-link');
 
 
 signOutLink.addEventListener('click', signOutUser);
 
 
-async function displayAllUsers() {
-    const users = await getAllUsers();
-
-    for (let user of users) {
-        const userDiv = renderAllUsers(user);
-        userContainerEl.append(userDiv);
-    }
-
-}
 
 
 
-async function loadPawfile() {
-    const pawfile = await getUserById(user.id);
-    if (pawfile && pawfile.image_url) {
-        const pawfileImg = document.querySelector('.pawfile-image');
-        console.log(pawfileImg);
-        pawfileImg.src = pawfile.image_url;
-    }
-}
 
-displayAllUsers();
-loadPawfile();
+
+
+
+
+// async function displayAllUsers() {
+//     const users = await getAllUsers();
+
+//     for (let user of users) {
+//         const userDiv = renderAllUsers(user);
+//         userContainerEl.append(userDiv);
+//     }
+
+// }
+
+
+
+// async function loadPawfile() {
+//     const pawfile = await getUserById(user.id);
+//     if (pawfile && pawfile.image_url) {
+//         const pawfileImg = document.querySelector('.pawfile-image');
+//         console.log(pawfileImg);
+//         pawfileImg.src = pawfile.image_url;
+//     }
+// }
+
+// displayAllUsers();
+// loadPawfile();
